32045e2d39cf8b9952e9f92a27a127ce53ddc71c
1emrecakici <1emrecakici@gmail.com>
2022-04-22 09:18:55 +0300

    Last commit is this project.

--- app/src/main/java/com/example/orderapps/MainActivity.kt
+++ app/src/main/java/com/example/orderapps/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_order.*
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Email Patterninin oluşması
         var emailPattern = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                 "\\@" +
                 "yalova.edu" +
@@ -27,38 +31,60 @@ class MainActivity : AppCompatActivity() {
                 "tr" +
                 ")+"
 
+        //Button ile gerekli kontrolleri yapıp diğer sayfaya geçiş sağlama
         btn_kayitOl.setOnClickListener {
-            // Check Email Adress
-            if (edt_email.text.toString().trim { it <= ' ' }.matches(emailPattern.toRegex())) {
-                // Check Password
-                if (edt_password.text.toString().length >= 8) {
-                    //Check Checkbox Click
-                    if(checkBox_kvkk.isChecked){
-                        val name = edt_name.text.toString()
-                        val surname = edt_surname.text.toString()
-                        val email = edt_email.text.toString()
-                        val newIntent = Intent(this@MainActivity,OrderActivity::class.java)
+            //Adın girileceği yerin dolu yada boş mu kontrol edilmesi
+            if (edt_name.text.toString().length > 2) {
+                //Soyadın girileceği yerin dolu yada boş mu kontrol edilmesi
+                if (edt_surname.text.toString().length > 2) {
+                    // Email Adresin kontrolü
+                    if (edt_email.text.toString().trim { it <= ' ' }
+                            .matches(emailPattern.toRegex())) {
+                        // Şifrenin Kontrolü
+                        if (edt_password.text.toString().length >= 8) {
+                            // Checkbox ın kontrolü
+                            if (checkBox_kvkk.isChecked) {
+                                //Ad ve Soyadın diğer sayfaya aktarımı
+                                val name = edt_name.text.toString()
+                                val surname = edt_surname.text.toString()
+                                val email = edt_email.text.toString()
+                                val newIntent = Intent(this@MainActivity, OrderActivity::class.java)
 
-                        newIntent.putExtra("name",name)
-                        newIntent.putExtra("surname", surname)
-                        newIntent.putExtra("email",email)
-                        startActivity(newIntent)
+                                newIntent.putExtra("name", name)
+                                newIntent.putExtra("surname", surname)
+                                newIntent.putExtra("email", email)
+                                startActivity(newIntent)
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    "KVKK Metnini Okuyunuz ve Onaylayınız...",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Şifreniz 8 Karakterden Az Olmamalı...",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Geçersiz Email Adresi...",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
-                    else {
-                        Toast.makeText(this, "Please check KVKK Clarification Text.", Toast.LENGTH_LONG).show()
-                    }
-                } else{
-                    Toast.makeText(applicationContext, "Your password must be more 8 characters", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext, "Soyisminizi Giriniz...", Toast.LENGTH_SHORT)
+                        .show()
                 }
-            }else {
-                Toast.makeText(applicationContext, "Invalid email address", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "İsminizi Giriniz...", Toast.LENGTH_SHORT).show()
             }
-            }
-
         }
-
-
     }
+}
 
 
 
--- app/src/main/java/com/example/orderapps/OrderActivity.kt
+++ app/src/main/java/com/example/orderapps/OrderActivity.kt
@@ -19,20 +19,25 @@ class OrderActivity : AppCompatActivity() {
         var sum1:Int = 0
         var sum2:Int = 0
 
+        //Ad ve Soyadın diğer sayfadan alınması
         val name = intent.getStringExtra("name")
         val surname = intent.getStringExtra("surname")
         val email = intent.getStringExtra("email")
 
-        profileFullName.text ="HOŞGELDİNİZ"+" "+name+" "+surname
+        // Ad ve Soyadın textview a yazdırılması
+        profileFullName.text =name+" "+surname
         profileUserEmail.text = email
 
+        //Glide fonkssiyonu ile fotoğraf çekilmesi
         Glide.with(this)
             .load("https://previews.123rf.com/images/triken/triken1608/triken160800029/61320775-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg")
             .centerCrop()
             .into(profile_image)
 
-        sum_txtview.setVisibility(View.INVISIBLE)
+        //Textview ın görünürülüğü
+        sum_txtview.setVisibility(View.GONE)
 
+        //Radio buttonların click kontrolleri
         rgrup_iskender.setOnCheckedChangeListener{iskender, checkedId ->
             if (checkedId == R.id.iskender10 || checkedId == R.id.iskender20 || checkedId == R.id.iskender30){
                 if (checkedId == R.id.iskender10){
@@ -60,6 +65,7 @@ class OrderActivity : AppCompatActivity() {
             }
         }
 
+        //Button ile toplam maliyeti yazdırma
         sum_button.setOnClickListener{
             if (sum1 == 0 || sum2 ==0){
                 if (sum1 == 0){
@@ -80,11 +86,6 @@ class OrderActivity : AppCompatActivity() {
                 sum1 = 0
                 sum2 = 0
             }
-
-
-
         }
-
-
     }
 }
